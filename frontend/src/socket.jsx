63dde397396eb018e30085fe0a9f7655872763ab
Shrey raj <120704020+shrey423@users.noreply.github.com>
2025-03-15 14:20:07 +0530

    change socket file

--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,12 +1,11 @@
 import { io } from 'socket.io-client';
 
-const socket = process.env.NODE_ENV === 'production'
-  ? io()
-  : io(`${import.meta.env.VITE_BASE_URL}`, {
-      withCredentials: true,
-      autoConnect: true,
-      reconnectionAttempts: 5,
-      reconnectionDelay: 3000
-    });
+const socket = io(`${import.meta.env.VITE_BASE_URL}`, {
+  withCredentials: true,
+  transports: ['websocket', 'polling'], // Explicitly set transports
+  autoConnect: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 3000
+});
 
 export default socket;
